5fd95f09ec14b55cf30ff37b2fb666e3ac5314f1
Clive Kifumbi <168200287+clivedev88@users.noreply.github.com>
2025-03-06 06:38:34 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,11 +5,9 @@ function toggleMode() {
     const img = document.querySelector("#profile img");
 
     if (html.classList.contains("light")) {
-        // Mudar para avatar no modo light
         img.setAttribute("src", "./assets/avatar-light.png");
         img.setAttribute("alt", "Foto de Clive Kifumbi com fundo roxo e azul para perfil");
     } else {
-        // Mudar para avatar no modo dark
         img.setAttribute("src", "./assets/avatar.png");
         img.setAttribute("alt", "Foto de Clive Kifumbi com fundo amarelo para perfil");
     }
